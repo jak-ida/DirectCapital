0660a9ee3aae67552a908876d544469ff15cf80f
Jakida <117814088+jak-ida@users.noreply.github.com>
2024-05-21 18:20:55 +0200

    Add files via upload

---
+++ JS/directjs.js
@@ -0,0 +1,18 @@
+function RedirectToHome(){
+    location.href="index.html"
+}
+function RedirectToAbout(){
+    location.href="about.html"
+}
+function RedirectToServices(a){
+    location.href="services.html#" + a;
+}
+function RedirectToContact(){
+    location.href="contact.html"
+}
+function RedirectToDownload(){
+    location.href="#"
+}
+
+
+
