2a830f60e98be4a6c22f216dce8ccb35dda2b252
Jakida <117814088+jak-ida@users.noreply.github.com>
2024-05-31 00:14:20 +0200

    Update directjs.js

--- directjs.js
+++ directjs.js
@@ -14,5 +14,19 @@ function RedirectToDownload(){
     location.href="#"
 }
 
+     
+// Show scroll-to-top button after scrolling halfway through the page
+window.onscroll = function () {
+    var btn = document.getElementById("scroll-to-top-btn");
+    if (document.body.scrollTop > (document.body.scrollHeight / 9) || document.documentElement.scrollTop > (document.documentElement.scrollHeight / 5)) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+};
 
-
+// Scroll to top when button is clicked
+document.getElementById("scroll-to-top-btn").addEventListener("click", function () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
