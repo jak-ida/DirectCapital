486b2c13fff4eb5f531a0a2e4c87e54509692c11
Jakida <117814088+jak-ida@users.noreply.github.com>
2024-07-03 21:05:37 +0200

    Update Snackbar.js

--- JS/Snackbar.js
+++ JS/Snackbar.js
@@ -36,7 +36,7 @@ function showSuccessToast(Title , Message) {
   }
 
 
-function toast({ title = "", message = "", type = "info", duration = 3000 }) {
+function toast({ title = "", message = "", type = "info", duration = 5000 }) {
     const main = document.getElementById("toast");
     if (main) {
       const toast = document.createElement("div");
@@ -63,9 +63,9 @@ function toast({ title = "", message = "", type = "info", duration = 3000 }) {
       const icon = icons[type];
       const delay = (duration / 1000).toFixed(2);
   
-      toast.classList.add("toast", `toast--${type}`);
+      toast.classList.add("Stoast", `toast--${type}`);
       toast.style.animation = `slideInLeft ease .3s, fadeOut linear 1s ${delay}s forwards`;
-  
+      
       toast.innerHTML = `
                       <div class="toast__icon">
                           <i class="${icon}"></i>
